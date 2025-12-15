dc487fcd2ea268898f098bb9f9fd5caf41488d5c
Valeria Avalos <avalosvaleria1006@gmail.com>
2025-12-15 16:42:34 -0300

    ultimo intento

--- config/database.php
+++ config/database.php
@@ -56,11 +56,12 @@ return [
             'prefix' => '',
             'strict' => true,
             'engine' => null,
-            'options' => extension_loaded('pdo_mysql') ? array_filter([
-                    PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,
-                ]) : [],
 
+            'options' => extension_loaded('pdo_mysql') ? array_filter([
+                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+            ]) : [],
         ],
+
 
 
 
